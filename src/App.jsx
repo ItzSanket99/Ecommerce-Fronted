cd19406fed10b914ff8c8f5d6c5497d241b7cdc8
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-08-10 10:52:15 +0530

    Setup Layout of  Product Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import { FaBeer } from 'react-icons/fa'
 import './App.css'
+import Products from './Components/Products'
 
 function App() {
 
   return (
-    <h1
-      className='flex items-center justify-center h-screen bg-blue-950 text-white font-bold text-4xl '
-      >Welcome  <FaBeer /></h1>
+    <>
+      <Products />
+    </>
   )
 }
 
