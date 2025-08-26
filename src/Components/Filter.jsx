036d7b085d5ca420a299238089f79cd0d79ca5e8
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-08-26 10:54:00 +0530

    update the URL Parameters based on User Selection

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,7 +1,8 @@
 import { Button, FormControl, InputLabel, MenuItem, Select, Tooltip } from '@mui/material';
-import React, { useState } from 'react'
-import { FaArrowUp, FaSearch } from 'react-icons/fa';
-import { MdRefresh } from 'react-icons/md';
+import React, { useEffect, useState } from 'react'
+import { FaArrowDown, FaArrowUp, FaSearch } from 'react-icons/fa';
+import { MdRefresh, MdTurnedInNot } from 'react-icons/md';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 
 
 const Filter = () => {
@@ -14,14 +15,70 @@ const Filter = () => {
     ]
 
     const [category, setCategory] = useState("all");
+    const [sortOrder, setSortOrder] = useState("asc");
+    const [searchTerm, setSearchTerm] = useState("");
+
     const handleCategoryChange = (event) => {
+        const selectedCategory = event.target.value;
+        if(selectedCategory === "all"){
+            params.delete("category");
+        }else{
+            params.set("category", selectedCategory);
+        }
+        navigate(`${pathName}?${params}`);
         setCategory(event.target.value);
     };
+
+    const toggleSortOrder = () => {
+        setSortOrder((prevOrder)=>{
+            const newOrder = (prevOrder === "asc") ? "desc" : "asc";
+            params.set("sortby",newOrder);
+            navigate(`${pathName}?${params}`);
+            return newOrder;
+        });
+    };
+
+    const handleClearFilter = () => {
+        navigate({ pathname : window.location.pathname })
+    };
+
+    const [searchParams] = useSearchParams();
+    const params = new URLSearchParams(searchParams);
+    const pathName = useLocation().pathname;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const currentCategory = searchParams.get("category") || "all";
+        const currentSortOrder = searchParams.get("sortby") || "asc";
+        const currentSearchTerm = searchParams.get("keyword") || "";
+
+        setCategory(currentCategory);
+        setSortOrder(currentSortOrder);
+        setSearchTerm(currentSearchTerm);
+
+    }, [searchParams])
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            if (searchTerm) {
+                searchParams.set("keyword",searchTerm);
+            } else {
+                searchParams.delete("keyword");
+            }
+            navigate(`${pathName}?${searchParams.toString()}`)
+        }, 700);
+        return () => {
+            clearTimeout(handler);
+        }
+    },[searchParams, searchTerm, navigate, pathName]);
+
   return (
     <>
         <div className='flex lg:flex-row flex-col-reverse lg:justify-between justify-center items-center gap-4' > 
             <div className=' relative flex items-center 2xl:w-[450px] xl:w-[420px] w-full' > 
                 <input type="text" placeholder='Search products' className=' border border-gray-400  text-slate-800 rounded-md py-2 pl-10 pr-4 w-full focus:outline-none focus:ring-2 focus:ring-[#1976d2]' 
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <FaSearch className=' absolute left-3 text-slate-700 size={20}'/>
             </div>
@@ -43,18 +100,29 @@ const Filter = () => {
                 </FormControl>
 
                 <Tooltip title="Sorted by price: asc">
-                    <Button variant="contained" color="primary">
+                    <Button 
+                        variant="contained" 
+                        color="primary"
+                        onClick={toggleSortOrder}
+                        >
                         Sort By
-                        <FaArrowUp size={20} className='pl-2' />
+                        {sortOrder === "asc" ? (
+                            <FaArrowUp size={20} className='pl-2' />
+                        ) : (
+                            <FaArrowDown size={20} className='pl-2' />
+                        )}
+                        
                     </Button>
                 </Tooltip>
-                <button className='flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-none' >
+                <button className='flex items-center gap-2 bg-rose-900 text-white px-3 py-2 rounded-md transition duration-300 ease-in shadow-md focus:outline-none'
+                onClick={handleClearFilter}
+                >
                     <MdRefresh className='font-semibold' size={16} />
                     <span className='font-semibold' >Clear Filter</span>
                 </button>
             </div>
         </div>
-    </>
+    </> 
   )
 }
 
