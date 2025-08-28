5a09ae1c669f1aff76cd36ec341f75fe4e9d99d9
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-08-28 16:33:02 +0530

    build about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Components/home/Home'
 import Products from './Components/Products/Products'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Shared/Navbar'
+import About from './Components/Shared/About'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path = '/' element = { <Home /> } />
           <Route path= '/products' element = { <Products /> } />
+          <Route path= '/about' element = { <About /> } />
         </Routes>
       </Router>  
     </>
