88697940c943873834a6c876159f228deb016893
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-08-28 15:56:51 +0530

    Set up navbar made it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Home from './Components/home/Home'
 import Products from './Components/Products/Products'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Shared/Navbar'
 
 function App() {
 
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route path = '/' element = { <Home /> } />
           <Route path= '/products' element = { <Products /> } />
