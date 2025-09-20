6feba2af3612d208c0bf1b0b25ef69d5c64faa94
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-09-20 11:22:34 +0530

    update navbar and display user menu if authenticated

---
+++ src/Components/Shared/UserMenu.jsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Avatar } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { BiUser } from "react-icons/bi";
+import { useSelector } from 'react-redux';
+import { FaShoppingCart } from 'react-icons/fa';
+import { IoExitOutline } from "react-icons/io5";
+import BackDrop from './BackDrop';
+
+const UserMenu = () => {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const logOutHandler = () => {
+
+  }
+
+  const {user} = useSelector((state) => state.auth)
+
+  return (
+    <div className=' relative z-30' >
+      <div
+        className='sm:border-[1px] sm:border-slate-400 flex flex-row items-center gap-1 rounded-full cursor-pointer hover:shadow-md transition text-slate-700'
+        onClick={handleClick}
+      >
+        <Avatar alt='menu' src=''/>  
+      </div>
+      <Menu
+        sx={{width:"400px" }}
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <Link to="/profile">
+            <MenuItem className='flex gap-2 items-center' onClick={handleClose}>
+                <BiUser className='text-xl' />  
+                <span className='font-bold text-[16px]' >
+                    {user?.username}
+                </span>
+            </MenuItem>
+        </Link>
+        <Link to="/profile/orders">
+            <MenuItem className='flex gap-2 items-center' onClick={handleClose}>
+                <FaShoppingCart className='text-xl' />  
+                <span className='font-semibold' >
+                    Order
+                </span>
+            </MenuItem>
+        </Link>
+        <MenuItem className='flex gap-2 items-center' onClick={logOutHandler}>
+            <div className='font-semibold w-full flex gap-2 items-center justify-center bg-button-gradient px4 py-1 text-white rounded-sm'>
+                <IoExitOutline className='text-xl' /> 
+                <span className='font-semibold' >
+                    Logout
+                </span>
+            </div>  
+        </MenuItem>
+  
+      </Menu>
+      {open && <BackDrop />}
+    </div>
+  );
+}
+
+export default UserMenu
