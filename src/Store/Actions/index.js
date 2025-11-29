570107c3a63f977d5c7d3d5c2f8401bb174015da
ItzSanket99 <161996340+ItzSanket99@users.noreply.github.com>
2025-11-29 20:42:51 +0530

    fetch analytics data from the backend and build interactive admin dashboard

--- src/Store/Actions/index.js
+++ src/Store/Actions/index.js
@@ -304,6 +304,21 @@ export const stripePaymentConfirmation =
             
         } catch (error) {
           toast.error("Payment Failed. Please try agian.");
-        } 
-       
+        }     
+}
+export const analyticsAction = () => async (dispatch, getState) => {
+        try {
+            dispatch({type : "IS_FETCHING"});
+            const {data} = await api.get("/admin/app/analytics");
+            dispatch({
+                type : "FETCH_ANALYTICS",
+                payload : data
+            });
+            dispatch({type: "IS_SUCCESS"});
+        } catch (error) {
+            dispatch({
+                type:"IS_ERROR",
+                payload:error?.response?.data?.message || "failed to fetch analytics data"
+            })
+        }     
 }
